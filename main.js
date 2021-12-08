6e691b5e572db1e1b19ca6aa83eb309cdd77a403
VARVISHNU08 <91448991+VARVISHNU08@users.noreply.github.com>
2021-12-08 13:11:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var first_name = "Vardhan";
+var last_name = "Burse";
+
+console.log("This is my first console project");
+console.log(first_name.concat(last_name));
+
+function calc(){
+    console.log(document.getElementById("a").value * document.getElementById("b").value);
+    console.log(document.getElementById("a").value / document.getElementById("b").value);
+    console.log(document.getElementById("a").value - document.getElementById("b").value);
+    console.log(document.getElementById("a").value + document.getElementById("b").value);
+    document.getElementById("how").style.display = "inline-block";
+}
+function intr(){
+    document.getElementById("info").style.display = "inline-block";
+}
